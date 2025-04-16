1a46afebd7e3beb368aa95a90d44dae8e8fd22b9
Eran Moshe <ernmse@gmail.com>
2025-04-16 18:15:10 +0300

    maman13 keep goin

--- dnn/maman13/src/maman13.py
+++ dnn/maman13/src/maman13.py
@@ -1,4 +1,6 @@
 import os
+from enum import Enum
+
 import pandas as pd
 import numpy as np
 import torch
@@ -19,13 +21,18 @@ data_path = os.path.abspath(os.path.join(__file__, '../..', 'data'))
 diabetes_path = os.path.join(data_path, 'diabetes.csv')
 
 
+class Task(str, Enum):
+    CLASSIFICATION = 'classification'
+    REGRESSION = 'regression'
+
+
 class ANeuralNetwork(nn.Module):
     def __init__(self, task, input_dim, output_dim):
         super().__init__()
         task = task.lower()
         assert task in AVB_TASKS, f"task: {task} should be either {AVB_TASKS}"
         self.output_dim = output_dim
-        if task == 'regression':
+        if task == Task.REGRESSION:
             self.output_dim = 1
         self.backbone = nn.Sequential(
             nn.Linear(input_dim, 32),
@@ -40,19 +47,25 @@ class ANeuralNetwork(nn.Module):
             nn.Linear(16, 16),
             nn.ReLU(),
         )
-        if task == 'regression':
-            self.out = nn.Linear(16, 1)
-        elif task == 'classification':
 
-            self.out = nn.Sequential(
-                nn.Linear(16, self.output_dim),
-                nn.Softmax(dim=1)
-            )
+        self.out = nn.Linear(16, self.output_dim)
 
     def forward(self, x):
         x = self.backbone(x)
-        pred = self.out(x)
-        return pred
+        return self.out(x)
+
+    def predict_proba(self, x):
+        logits = self.forward(x)
+        if self.task == Task.CLASSIFICATION:
+            return torch.softmax(logits, dim=1)
+        else:
+            raise ValueError("probabilities <==> classification")
+
+    def predict(self, x):
+        if self.task == Task.CLASSIFICATION:
+            return torch.argmax(self.forward(x), dim=1)
+        else:
+            return self.forward(x)
 
 
 class DiabetesTabularDataset(Dataset):
@@ -145,7 +158,7 @@ def train_model(
         valid_loader: DataLoader,
         task: str,
         epochs: int = 10,
-        lr: float = 3e-3,
+        lr: float = 3e-4,
         wd: float = 0.05):
     task = task.lower()
     assert task in AVB_TASKS, f"{task} should be in {AVB_TASKS}"
@@ -173,7 +186,7 @@ def train_model(
         avg_val_loss = validation_loop(model, valid_loader, loss_fn, task)
 
 
-def question_8(n_cuts=10, task='classification', epochs=1000):
+def question_8(n_cuts=10, exclude_Y=False, task='classification', epochs=500):
     """ We create our x using Y, and pred is Class `exclude_Y=False`
     Also notice that n_cuts determine amount of classes (and our output dim) """
     diabetes_df = preprocess_df(diabetes_path, n_cuts)
@@ -183,10 +196,10 @@ def question_8(n_cuts=10, task='classification', epochs=1000):
     train_diab_df, valid_diab_df = train_test_split(
         diabetes_df, test_size=0.2, random_state=RANDOM_STATE)
 
-    diab_training_ds = DiabetesTabularDataset(train_diab_df, exclude_Y=False)
+    diab_training_ds = DiabetesTabularDataset(train_diab_df, exclude_Y=exclude_Y)
     diab_training_dl = DataLoader(diab_training_ds, batch_size=10, shuffle=True)
 
-    diab_validation_ds = DiabetesTabularDataset(valid_diab_df, exclude_Y=False)
+    diab_validation_ds = DiabetesTabularDataset(valid_diab_df, exclude_Y=exclude_Y)
     diab_validation_dl = DataLoader(diab_validation_ds, batch_size=10, shuffle=True)
 
     x, y_reg, y_cat = next(iter(diab_training_dl))
