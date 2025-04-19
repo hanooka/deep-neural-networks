05c5c92f87d37b6d72f9093e5027f21538da8f87
Eran <ernmse@gmail.com>
2025-04-19 11:52:29 +0300

    13 finito

--- dnn/maman13/src/maman13.py
+++ dnn/maman13/src/maman13.py
@@ -44,10 +44,10 @@ class ANeuralNetwork(nn.Module):
         self.backbone = nn.Sequential(
             nn.Linear(input_dim, 1024),
             nn.ReLU(),
-            nn.Dropout(0.25),
+            nn.Dropout(0.3),
             nn.Linear(1024, 1024),
             nn.ReLU(),
-            nn.Dropout(0.25),
+            nn.Dropout(0.3),
             nn.Linear(1024, 1024),
             nn.ReLU(),
         )
@@ -135,11 +135,12 @@ def questions_6_7():
 
 
 def validation_loop(model, val_loader, loss_fn, task) -> (float, float):
-    """ """
+    """ run validation on val_loader, using loss_fn and metric using metric map and task.
+    returns the avg_loss, and metric value. """
     val_loss = 0.
     assert task in AVB_TASKS, f"{task} should be in {AVB_TASKS}"
     metric, metric_name = metric_map[task]
-    metric = metric()
+    metric = metric(device=DEVICE)
     model.eval()
     with torch.no_grad():
         for x, y_reg, y_cls in val_loader:
@@ -159,6 +160,8 @@ def train_model(
         valid_loader: DataLoader,
         task: Task,
         epochs: int = 10,
+        verbose: int = 1,
+        verbose_batch: int = 1,
         lr: float = 1e-4,
         wd: float = 0.05) -> nn.Module:
     """
@@ -171,6 +174,8 @@ def train_model(
     :param valid_loader:
     :param task: Task (currently 'classification' or 'regression')
     :param epochs:
+    :param verbose: [0, 1, 2] Level of printing information (0 None, 2 Max)
+    :param verbose_batch: if verbose is 2, how many batches before printing metrices and loss.
     :param lr: learning rate
     :param wd: weight decay
     :return: a model
@@ -179,12 +184,11 @@ def train_model(
     assert task in AVB_TASKS, f"{task} should be in {AVB_TASKS}"
     loss_fn = loss_fn_map[task]
     metric, metric_name = metric_map[task]
-    metric = metric()
+    metric = metric(device=DEVICE)
 
     opt = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=wd)
 
     for epoch in range(epochs):
-        print(f"Epoch [{epoch + 1}/{epochs}]", end=', ')
         running_loss = 0.
         model.train()
         metric.reset()
@@ -198,19 +202,37 @@ def train_model(
             opt.step()
             running_loss += loss.item()
 
-        #if i % 10 == 0:  # Print every 10 batches
+            # Print every `verbose_batch` batches
+            if verbose >= 2 and i % verbose_batch == 0:
+                print(f"Epoch [{epoch + 1}/{epochs}], "
+                      f"Step [{i}/{len(train_loader)}], "
+                      f"Loss: {loss.item():.4f}", sep=',')
 
         # End of epoch. Run validation and print outcomes
         avg_val_loss, metric_val = validation_loop(model, valid_loader, loss_fn, task)
-        print(f"trn los: {running_loss / (epoch + 1):.4f},", f"trn {metric_name}: {metric.compute():.4f}", end=', ')
-        print(f"val loss: {avg_val_loss:.4f}, val {metric_name}: {metric_val:.4f}")
+        if verbose >= 1:
+            print(f"Epoch [{epoch + 1:4}/{epochs}]", end=f", ")
+            print(f"trn los: {running_loss / (epoch + 1):8.4f},", f"trn {metric_name}: {metric.compute():6.4f}",
+                  end=', ')
+            print(f"val loss: {avg_val_loss:8.4f}, val {metric_name}: {metric_val:6.4f}")
 
     return model
 
 
-def general_solver(n_cuts=10, exclude_Y=False, task=Task.CLASSIFICATION, epochs=100):
-    """ TODO Fill this
-    notice that n_cuts determine amount of classes (and our output dim) """
+def general_solver(n_cuts=10,
+                   exclude_Y=False,
+                   task=Task.CLASSIFICATION,
+                   epochs=100, verbose=1,
+                   verbose_batch=10,
+                   lr: float = 1e-4):
+    """ we read the diabetes dataset, and create the "Class" column using n_cuts
+    We split to train and test (0.2)
+    We wrap the dataset with our custom DiabetesTabularDataset
+    and generate a DataLoader wrapping the dataset. batch_size is 10 and shuffle is True
+    We initialize (and instantiate) the neural network based on data (x.shape[1]) and n_cuts - which is
+    the output dim.
+    notice that n_cuts determine amount of classes (and our output dim)
+    We train, and watch resulted printed to screen. """
     diabetes_df = preprocess_df(diabetes_path, n_cuts)
 
     # Stratification should be considered here.
@@ -228,35 +250,57 @@ def general_solver(n_cuts=10, exclude_Y=False, task=Task.CLASSIFICATION, epochs=
 
     model = ANeuralNetwork(task=task, input_dim=x.shape[1], output_dim=n_cuts)
     model.to(DEVICE)
-    model = train_model(model, diab_training_dl, diab_validation_dl, task=task, epochs=epochs)
+    model = train_model(model, diab_training_dl, diab_validation_dl, task=task, epochs=epochs, verbose=verbose,
+                        verbose_batch=verbose_batch, lr=lr)
 
 
 def question_8():
     general_solver(n_cuts=10, exclude_Y=False, task=Task.CLASSIFICATION, epochs=100)
-    # Epoch [500/500], Step [30/36], Loss: 0.5692, Accuracy: 0.6314
-    # Val loss: 0.9871, Val Accuracy: 0.6314
+    # Epoch [   1/100], trn los: 190.9444, trn acc: 0.1530, val loss:   2.3679, val acc: 0.2472
+    # Epoch [ 100/100], trn los:   0.3279, trn acc: 0.5609, val loss:   0.9889, val acc: 0.5730
 
 
 def question_9():
     general_solver(n_cuts=10, exclude_Y=True, task=Task.CLASSIFICATION, epochs=500)
-    # Epoch [500/500], Step [30/36], Loss: 1.9232, Accuracy: 0.2884
-    # Val loss: 2.0823, Val Accuracy: 0.2360
+    # Epoch [   5/500], trn los:  21.1454, trn acc: 0.1133, val loss:   2.5425, val acc: 0.0787
+    # Epoch [ 500/500], trn los:   0.1397, trn acc: 0.2975, val loss:   2.0022, val acc: 0.2135
 
 
 def question_12():
-    general_solver(n_cuts=100, exclude_Y=False, task=Task.CLASSIFICATION, epochs=100)
-    # Epoch [500/500], Step [30/36], Loss: 0.6346, Accuracy: 0.5664
-    # Val loss: 7.5515, Val Accuracy: 0.5663
+    general_solver(n_cuts=100, exclude_Y=False, task=Task.CLASSIFICATION, epochs=200, lr=3e-5)
+    # We lowered the learning rate a bit here, We also increased the dropout % for this training session
+    # Epoch [   1/200], trn los: 470.0365, trn acc: 0.0057, val loss:   6.4315, val acc: 0.0225
+    # Epoch [ 200/200], trn los:   0.5688, trn acc: 0.1161, val loss:   3.5989, val acc: 0.1124
 
 
 def question_13():
-    general_solver(n_cuts=100, exclude_Y=True, task=Task.CLASSIFICATION, epochs=500)
-    # Epoch [500/500], Step [30/36], Loss: 0.5034, Accuracy: 0.5698
-    # Val loss: 11.0239, Val Accuracy: 0.5696
+    general_solver(n_cuts=100, exclude_Y=True, task=Task.CLASSIFICATION, epochs=200, lr=1e-4)
+    # Epoch [   1/200], trn los: 311.1128, trn acc: 0.0113, val loss:   5.0817, val acc: 0.0000
+    # Epoch [  67/200], trn los:   2.2001, trn acc: 0.0397, val loss:   4.9264, val acc: 0.0337
+    # Epoch [ 200/200], trn los:   0.3902, trn acc: 0.3399, val loss:   7.6031, val acc: 0.0000
+
+    # So we can see a beautiful overfit here. Where we started with 1% accuracy (which fits 100 cuts)
+    # At our best (around epoch 67, we can see the train acc is about 4% and validation at 3.3%)
+    # After that we overfit and get a great 34% accuracy which worth nothing because validation is 0.
+
+
+def question_14():
+    general_solver(n_cuts=1, exclude_Y=True, task=Task.REGRESSION, epochs=1000, lr=1e-4, verbose=1)
+    # Epoch [   1/1000], trn los: 331214.6368, trn r2: -0.5712, val loss: 5517.4431, val r2: 0.0567
+    # Epoch [ 100/1000], trn los: 1174.3142, trn r2: 0.4493, val loss: 3013.6842, val r2: 0.4818
+    # Epoch [ 728/1000], trn los: 121.1772, trn r2: 0.5838, val loss: 2487.0921, val r2: 0.5720
+    # Epoch [1000/1000], trn los:  73.8316, trn r2: 0.6607, val loss: 2743.2695, val r2: 0.5307
+
+    # We can see we start the train terrible with a huge loss and negative r2.
+    # In epoch 100, we can see we’ve improved and not overfitting yet (train and validation at the same level)
+    # In epoch 728 we can see we kept improving, and this is the maximum r2 and best epoch to stop.
+    # In epoch 1000 we can see the overfit with trn r2 >> val r2.
 
 
 def main():
-    question_9()
+    qs = [question_8, question_9, question_12, question_13, question_14]
+    for q in qs:
+        q()
 
 
 if __name__ == '__main__':
